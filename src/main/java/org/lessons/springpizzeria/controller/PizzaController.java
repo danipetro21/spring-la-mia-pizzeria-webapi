74f29dbae81098e5cfed769b916e93098d1caf1e
danipetro21 <idany346@gmail.com>
2023-06-26 15:13:33 +0200

    add function "addpizza" without validation

--- src/main/java/org/lessons/springpizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/springpizzeria/controller/PizzaController.java
@@ -1,16 +1,15 @@
 package org.lessons.springpizzeria.controller;
 
 
+import jakarta.validation.Valid;
 import org.lessons.springpizzeria.model.Pizza;
 import org.lessons.springpizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -26,9 +25,7 @@ public class PizzaController {
 
 
     @GetMapping
-    public String list(
-            @RequestParam(name = "keyword", required = false) String searchString,
-            Model model) {
+    public String list(@RequestParam(name = "keyword", required = false) String searchString, Model model) {
         List<Pizza> pizze;
 
         if (searchString == null || searchString.isBlank()) {
@@ -54,4 +51,21 @@ public class PizzaController {
                     "pizza with id " + pizzaId + " not found");
         }
     }
+
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("book") Pizza formBook, BindingResult bindingResult) {
+
+
+        pizzaRepository.save(formBook);
+
+        return "redirect:/";
+    }
+
 }
