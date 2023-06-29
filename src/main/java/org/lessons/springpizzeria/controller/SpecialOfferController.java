00aa2795c1e678430e315ac11ace4bc112afa549
danipetro21 <idany346@gmail.com>
2023-06-29 15:30:56 +0200

    add create

--- src/main/java/org/lessons/springpizzeria/controller/SpecialOfferController.java
+++ src/main/java/org/lessons/springpizzeria/controller/SpecialOfferController.java
@@ -1,15 +1,25 @@
 package org.lessons.springpizzeria.controller;
 
 
+import jakarta.validation.Valid;
+import org.lessons.springpizzeria.model.Pizza;
+import org.lessons.springpizzeria.model.SpecialOffer;
 import org.lessons.springpizzeria.repository.PizzaRepository;
 import org.lessons.springpizzeria.repository.SpecialOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/offers")
-public class SpecialOfferController{
+public class SpecialOfferController {
 
     @Autowired
     PizzaRepository pizzaRepository;
@@ -17,6 +27,30 @@ public class SpecialOfferController{
     SpecialOfferRepository specialOfferRepository;
 
 
+    @GetMapping("/create")
+    public String create(@RequestParam("pizzaId") Integer pizzaId, Model model) {
+        SpecialOffer offer = new SpecialOffer();
+        offer.setStart(LocalDate.now());
+        Optional<Pizza> pizza = pizzaRepository.findById(pizzaId);
+        if (pizza.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "pizza with id " + pizzaId + " not found");
+        }
+        offer.setPizza(pizza.get());
+        model.addAttribute("specialOffer", offer);
+        return "/offers/form";
+    }
 
+
+    @PostMapping("/create")
+    public String update(@Valid @ModelAttribute("specialOffer") SpecialOffer formOffer, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "/offers/form";
+        }
+        specialOfferRepository.save(formOffer);
+
+        return "redirect:/" + formOffer.getPizza().getId();
+    }
 
 }
