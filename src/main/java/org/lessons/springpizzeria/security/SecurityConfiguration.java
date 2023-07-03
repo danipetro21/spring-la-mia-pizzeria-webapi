61a7fc52c251254ce78f2cf7fc530a481b69c3c5
danipetro21 <idany346@gmail.com>
2023-07-03 15:14:47 +0200

    add authorization route + logout btn

--- src/main/java/org/lessons/springpizzeria/security/SecurityConfiguration.java
+++ src/main/java/org/lessons/springpizzeria/security/SecurityConfiguration.java
@@ -1,4 +1,53 @@
 package org.lessons.springpizzeria.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    DatabaseUserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(passwordEncoder());
+
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/ingredients").hasAuthority("ADMIN")
+                .requestMatchers("/create/**").hasAuthority("ADMIN")
+                .requestMatchers("/edit/**").hasAuthority("ADMIN")
+                .requestMatchers("/delete/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+                .requestMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().logout();
+        return http.build();
+
+    }
 }
